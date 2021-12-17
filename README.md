1fd1c47d8fb9f54fbfb3ff2beb38b6a1dbbaa4e0
xueyan-site <yang@xueyan.site>
2021-12-17 11:49:28 +0800

    chore: project created

---
+++ README.md
@@ -0,0 +1,28 @@
+# xueyan-react-icon
+
+`xueyan-react-icon` is a react package.  
+
+- [xueyan-react-icon](#xueyan-react-icon)
+  - [Why use it](#why-use-it)
+  - [What are its highlights](#what-are-its-highlights)
+  - [How to use it](#how-to-use-it)
+  - [Interface](#interface)
+  - [Develop](#develop)
+  - [Appendix](#appendix)
+
+## Why use it
+
+## What are its highlights
+
+## How to use it
+
+## Interface
+
+## Develop
+
+Please make sure that `Node` and `NPM` are installed on your computer, and `xueyan-typescript-cli` is installed globally. After switching current work path to this project root in CLI, run command `yarn start`.
+
+## Appendix
+
+author - xueyan-site <yang@xueyan.site>  
+builder - [xueyan-typescript-cli](https://github.com/xueyan-site/xueyan-typescript-cli)  
---
+++ doc/src/zh/intro.tsx
@@ -0,0 +1,38 @@
+import React, { useState, Fragment } from 'react'
+import { Article, Segment } from 'xueyan-react-markdown'
+import Playground from 'xueyan-react-playground'
+import { Switch } from 'xueyan-react-icon'
+
+const MARK1 = `
+## xueyan-react-icon
+
+\`xueyan-react-icon\` 是一个 React 组件。  
+
+## 用法
+`
+
+const code1 = `
+import React, { useState } from 'react'
+import Switch from 'xueyan-react-icon'
+
+export default function Example() {
+  const [state, setState] = useState<boolean>(false)
+  return (
+    <Fragment>
+      <span>switch: </span>
+      <Switch value={state} onChange={setState} />
+    </Fragment>
+  )
+}
+`
+
+export default function Main() {
+  return (
+    <Article>
+      <Segment>{MARK1}</Segment>
+      <Playground scope={{ React, useState, Fragment, Switch }}>
+        {code1}
+      </Playground>
+    </Article>
+  )
+}
